81e79f03e45cdd475ed2edb8ecba9cdde48f46eb
Nerijus Dulke <nerijus.dulke@devbridge.com>
2021-07-21 14:04:58 +0300

    add scrolling in the console windows

--- src/components/TaskRunner/TaskConsole.tsx
+++ src/components/TaskRunner/TaskConsole.tsx
@@ -10,7 +10,14 @@ const TaskConsole: React.FC<TaskConsoleProps> = ({ currentTaskId }) => {
   const lines = useTaskOutput(currentTaskId);
 
   return (
-    <Flex flexDirection="column" p={4} bg="black" w="100%">
+    <Flex
+      flexDirection="column"
+      p={4}
+      bg="black"
+      w="100%"
+      h="100vh"
+      overflow="auto"
+    >
       {lines.map((line) => (
         <Text as="code" color={line.color} key={line.id}>
           {line.text}
